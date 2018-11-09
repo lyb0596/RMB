e68b7f7bd54597ed2f982749692f02c0a9f77f88
lyb0596 <yibin_liu@163.com>
2018-11-09 16:22:47 +0800

    2test

---
+++ currency_1.0.py
@@ -0,0 +1,12 @@
+
+rmb_str_value = input('input: ')
+lenth = len(rmb_str_value)
+print(lenth)
+#value = rmb_str_value[lenth-3:lenth]
+value = rmb_str_value[-3:]
+print(value)
+# transe = 6.77
+# rmb_value = eval(rmb_str_value)
+
+# usa = rmb_value/transe
+# print(rmb_value,' && ',usa)
